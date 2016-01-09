a26bac0a528e9c35adebe9e757e1b7516c6a8faf
selfup <boudinot.regis@yahoo.com>
2016-01-09 15:14:00 -0700

    moldable fizzbuzz

--- Misc/fizz_buzz.py
+++ Misc/fizz_buzz.py
@@ -1,12 +1,14 @@
 class Fizz:
-    def __init__(self, range_for_n, first_word, second_word):
+    def __init__(self, range_for_n, n1, n2, first_word, second_word):
         self.range_for_n = (range_for_n + 1)
+        self.n1 = n1
+        self.n2 = n2
         self.first_word = first_word
         self.second_word = second_word
         self.biggest_word = ''.join([first_word, second_word])
 
     def print_i_condition(self, i, n, n2):
-        if (i % 5 != 0 and i % 3 != 0):
+        if (i % self.n2 != 0 and i % self.n1 != 0):
             print i
 
     def fizz_or_buzz_condition(self, i, n, n2, message):
@@ -14,20 +16,20 @@ class Fizz:
             print message
 
     def fizz_buzz_condition(self, i, n):
-        if (i % 15 == 0):
+        if (i % (self.n1 * self.n2) == 0):
             print self.biggest_word
 
     def fizz_buzz(self, i):
-        self.fizz_buzz_condition(i, 15)
+        self.fizz_buzz_condition(i, (self.n1 * self.n2))
 
     def fizz(self, i):
-        self.fizz_or_buzz_condition(i, 3, 5, self.first_word)
+        self.fizz_or_buzz_condition(i, self.n1, self.n2, self.first_word)
 
     def buzz(self, i):
-        self.fizz_or_buzz_condition(i, 5, 3, self.second_word)
+        self.fizz_or_buzz_condition(i, self.n2, self.n1, self.second_word)
 
     def print_i(self, i):
-        self.print_i_condition(i, 5, 3)
+        self.print_i_condition(i, self.n2, self.n1)
 
     def output(self):
         n = self.range_for_n
@@ -37,6 +39,6 @@ class Fizz:
             self.buzz(i)
             self.print_i(i)
 
-Fizz(15, 'Fizz', 'Buzz').output()
+Fizz(30, 3, 5, 'Fizz', 'Buzz').output()
 print '\n'
-Fizz(15, 'Hello', 'Goodbye').output()
+Fizz(30 , 3, 5, 'Hello', 'Goodbye').output()
