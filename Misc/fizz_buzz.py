da9aa8c114c25188ca90c44ee60515ac0e9eed09
Regis <boudinot.regis@yahoo.com>
2016-01-10 01:03:42 -0700

    slight refactor - removed uneeded params for some functions

--- Misc/fizz_buzz.py
+++ Misc/fizz_buzz.py
@@ -7,29 +7,29 @@ class Fizz:
         self.second_word = second_word
         self.biggest_word = ''.join([first_word, second_word])
 
-    def print_i_condition(self, i, n, n2):
+    def print_i_condition(self, i):
         if (i % self.n2 != 0 and i % self.n1 != 0):
             print i
 
-    def fizz_or_buzz_condition(self, i, n, n2, message):
-        if (i % n == 0 and i % n2 != 0):
+    def fizz_or_buzz_condition(self, i, message):
+        if (i % self.n1 == 0 and i % self.n2 != 0):
             print message
 
-    def fizz_buzz_condition(self, i, n):
+    def fizz_buzz_condition(self, i):
         if (i % (self.n1 * self.n2) == 0):
             print self.biggest_word
 
     def fizz_buzz(self, i):
-        self.fizz_buzz_condition(i, (self.n1 * self.n2))
+        self.fizz_buzz_condition(i)
 
     def fizz(self, i):
-        self.fizz_or_buzz_condition(i, self.n1, self.n2, self.first_word)
+        self.fizz_or_buzz_condition(i, self.first_word)
 
     def buzz(self, i):
-        self.fizz_or_buzz_condition(i, self.n2, self.n1, self.second_word)
+        self.fizz_or_buzz_condition(i, self.second_word)
 
     def print_i(self, i):
-        self.print_i_condition(i, self.n2, self.n1)
+        self.print_i_condition(i)
 
     def output(self):
         n = self.range_for_n
