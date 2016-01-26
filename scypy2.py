0fdeb8b7efc3e774be55623f2cd47822868c45cd
Regis <boudinot.regis@yahoo.com>
2016-01-26 16:55:02 -0700

    final ness

--- scypy2.py
+++ scypy2.py
@@ -11,18 +11,3 @@ o('brew install graphviz --with-librsvg --with-x11')
 o('brew install cairo')
 o('brew install py2cairo') # this will ask you to download xquartz and install it
 o('brew install qt pyqt')
-o('pip install virtualenv && pip install nose && pip install numpy && pip install scipy && brew install numpy && brew install scipy')
-o("python -c 'import numpy ; numpy.test();'")
-o("python -c 'import scipy ; scipy.test();'")
-# brew install matplotlib --with-cairo --with-tex
-# pip install pandas
-# pip install nltk
-# pip install sympy
-# pip install q
-# pip install snakeviz
-# brew install zmq
-# pip install ipython[all]
-# pip install html5lib cssselect pyquery lxml BeautifulSoup
-# pip install Flask Django tornado
-# pip install rdflib SPARQLWrapper
-# pip install networkx
--- scypy3.py
+++ scypy3.py
@@ -3,5 +3,14 @@ import os
 o = os.system
 
 o('pip install virtualenv && pip install nose && pip install numpy && pip install scipy && brew install numpy && brew install scipy')
+o('brew link numpy')
+o('brew link --overwrite numpy')
 o("python -c 'import numpy ; numpy.test();'")
 o("python -c 'import scipy ; scipy.test();'")
+o('# brew install matplotlib --with-cairo --with-tex && pip install pandas && pip install nltk && pip install sympy && pip install q && pip install snakeviz')
+o('brew install zmq')
+o('pip install ipython[all]')
+o('pip install html5lib cssselect pyquery lxml BeautifulSoup')
+o('pip install Flask Django tornado')
+o('pip install rdflib SPARQLWrapper')
+o('pip install networkx')
