611c096ddf5d94dfcb21ebc2a740073db12548f2
Regis <boudinot.regis@yahoo.com>
2016-01-26 16:46:43 -0700

    modified scypy3.py

--- scypy3.py
+++ scypy3.py
@@ -1,3 +1,7 @@
+import os
+
+o = os.system
+
 o('pip install virtualenv && pip install nose && pip install numpy && pip install scipy && brew install numpy && brew install scipy')
 o("python -c 'import numpy ; numpy.test();'")
 o("python -c 'import scipy ; scipy.test();'")
